f1ce14704306ab27ff54bb3f1bd18ce149f42994
Maham Mehrabi <maruga82@gmail.com>
2020-10-16 12:48:04 +0200

    push sito

---
+++ precache-manifest.dc33de1514007b95dab2bd0a7d96f058.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090a4f9f967a78d64725a7636cdead15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f30ff14e7fa87691592e",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f30ff14e7fa87691592e",
+    "url": "/static/js/main.8b33f832.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
